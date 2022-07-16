71b06aad9c4211c6823b24550af3381a492d4a7c
GeorgeHuan <george.huan1994@foxmail.com>
2022-07-16 15:59:41 +0800

    Update

--- Assets/URP/ComputeShaderParticles/Particles.shader
+++ Assets/URP/ComputeShaderParticles/Particles.shader
@@ -1,14 +1,12 @@
 ﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
 
-// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
-
 Shader "Subway/Particles"
 {
 
     Properties
     {
         _ColorLow("Color Slow Speed", Color) = (0, 0, 0.5, 1)
-        _ColorHigh("Color High Speed", Color) = (1, 0, 0, 1)
+        _ColorHigh("Color High Speed", Color) = (1, 0.215, 0, 1)
         _HighSpeedValue("High speed Value", Range(0, 50)) = 25
     }
 
@@ -26,47 +24,44 @@ Shader "Subway/Particles"
 
             #include "UnityCG.cginc"
 
-            // Particle's data
             struct Particle
             {
                 float2 position;
                 float2 velocity;
             };
 
-            // Pixel shader input
-            struct PS_INPUT
+            struct Attributes
             {
                 float4 position : SV_POSITION;
                 float4 color : COLOR;
             };
 
-            // Particle's data, shared with the compute shader
+            // 接收 Compute Buffer 共享的粒子数据
             StructuredBuffer<Particle> Particles;
 
-            // Properties variables
             uniform float4 _ColorLow;
             uniform float4 _ColorHigh;
             uniform float _HighSpeedValue;
 
-            // Vertex shader
-            PS_INPUT vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID)
+            Attributes vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID)
             {
-                PS_INPUT o = (PS_INPUT)0;
+                Attributes OUT = (Attributes)0;
 
-                // Color
+                // 获取粒子速度，插值获得粒子颜色
                 float speed = length(Particles[instance_id].velocity);
                 float lerpValue = clamp(speed / _HighSpeedValue, 0.0f, 1.0f);
-                o.color = lerp(_ColorLow, _ColorHigh, lerpValue);
-                // Position
-                o.position = UnityObjectToClipPos(float4(Particles[instance_id].position, 0.0f, 1.0f));
+                OUT.color = lerp(_ColorLow, _ColorHigh, lerpValue);
+                
+                // 位置
+                OUT.position = UnityObjectToClipPos(float4(Particles[instance_id].position, 0.0f, 1.0f));
 
-                return o;
+                return OUT;
             }
 
-            // Pixel shader
-            float4 frag(PS_INPUT i) : COLOR
+
+            float4 frag(Attributes IN) : COLOR
             {
-                return i.color;
+                return IN.color;
             }
 
             ENDCG
--- Assets/URP/ComputeShaderParticles/ParticlesManager.cs
+++ Assets/URP/ComputeShaderParticles/ParticlesManager.cs
@@ -8,22 +8,94 @@ public class ParticlesManager : MonoBehaviour {
     /// </summary>
     public struct Particle
     {
-        public Vector2 position;
-        public Vector2 velocity;
+        public Vector2 position;    // 粒子位置
+        public Vector2 velocity;    // 粒子速度
     }
+    
+    private int _kernelIndex;                   // kernel 索引
 
-    public ComputeShader computeShader;         // 与着色器共用的 Compute Buffer
+    public ComputeShader computeShader;
     public Material material;                   // 材质
     public int particlesCount = 1024000;        // 粒子数量
     
-    private ComputeBuffer _particlesBuffer;
-    private int _particleBufferSize;            // Particle 数据大小
-    private Particle[] _particleBufferData;     // 要传递给 Compute Buffer 的数据
+    private ComputeBuffer _computeBuffer;       // Compute Buffer
+    private int _particleBufferSize;            // 单个 Particle 结构体的占用大小
+    private Particle[] _particleBufferData;     // 初始 Particle 数据
     
-    private const int WARP_SIZE = 1024;         // 单个线程组中的线程数
-    private int _warpCount;                     // 所需线程组数
+    private const int WarpSize = 1024;          // 单个线程组中的线程数
+    private int _warpCount;                     // 所需线程组的数量
+    
+    void Start () 
+    {
+        _kernelIndex = computeShader.FindKernel("Update");
+        
+        _particleBufferSize = Marshal.SizeOf(typeof(Particle));
 
-    private int _kernelIndex;                   // kernel 索引
+        _computeBuffer = new ComputeBuffer(particlesCount, _particleBufferSize, ComputeBufferType.Default);
+
+        _particleBufferData = new Particle[particlesCount];
+        
+        for (int i = 0; i < particlesCount; i++)
+        {
+            _particleBufferData[i] = new Particle
+            {
+                position = Random.insideUnitCircle * 10f,   // 位置：在半径为 10 的圆内随机
+                velocity = Vector2.zero
+            };
+        }
+
+        _computeBuffer.SetData(_particleBufferData);
+        
+        // 传递 Compute Buffer 到 Compute Shader 当中
+        computeShader.SetBuffer(_kernelIndex, "Particles", _computeBuffer);
+
+        // 传递 Compute Buffer 到 Shader 当中
+        material.SetBuffer("Particles", _computeBuffer);
+        
+        // 应当总是将线程组的数量设置为 warp size 的整数倍
+        _warpCount = Mathf.CeilToInt((float)particlesCount / WarpSize);
+    }
+    
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _computeBuffer.SetData(_particleBufferData);
+        }
+
+        computeShader.SetInt("shouldMove", Input.GetMouseButton(0) ? 1 : 0);
+        var mousePosition = GetMousePosition();
+        computeShader.SetFloats("mousePosition", mousePosition);
+        computeShader.SetFloat("deltaTime", Time.deltaTime);
+        
+        computeShader.Dispatch(_kernelIndex, _warpCount, 1, 1);
+	}
+    
+    void OnRenderObject()
+    {
+        // 激活给定的 Pass 以进行渲染
+        material.SetPass(0);
+        
+        // 在 GPU 上执行 Draw Call，第一个参数是拓扑结构，第二个参数数顶点数，第三个参数是实例数
+        Graphics.DrawProceduralNow(MeshTopology.Points, 1, particlesCount);
+    }
+
+    void OnDestroy()
+    {
+        if (_computeBuffer != null)
+            _computeBuffer.Release();
+    }
+    
+    /// <summary>
+    /// 获取鼠标的屏幕坐标
+    /// </summary>
+    /// <returns></returns>
+    float[] GetMousePosition()
+    {
+        var mp = Input.mousePosition;
+        var v = Camera.main.ScreenToWorldPoint(mp);
+        return new float[] { v.x, v.y };
+    }
     
     /*private void OnDrawGizmos()
     {
@@ -34,62 +106,4 @@ public class ParticlesManager : MonoBehaviour {
             Gizmos.DrawSphere(p[i].position, 0.1f);
         }
     }*/
-    
-    void Start () {
-
-        _warpCount = Mathf.CeilToInt((float)particlesCount / WARP_SIZE);
-
-        _particleBufferSize = Marshal.SizeOf(typeof(Particle));
-        _particlesBuffer = new ComputeBuffer(particlesCount, _particleBufferSize);
-
-        _particleBufferData = new Particle[particlesCount];
-
-        for (int i = 0; i < particlesCount; i++)
-        {
-            _particleBufferData[i] = new Particle();
-            _particleBufferData[i].position = Random.insideUnitCircle * 10f;
-            _particleBufferData[i].velocity = Vector2.zero;
-        }
-
-        _particlesBuffer.SetData(_particleBufferData);
-
-        _kernelIndex = computeShader.FindKernel("Update");
-        
-        computeShader.SetBuffer(_kernelIndex, "Particles", _particlesBuffer);
-
-        material.SetBuffer("Particles", _particlesBuffer);
-    }
-    
-	void Update () {
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            _particlesBuffer.SetData(_particleBufferData);
-        }
-
-        computeShader.SetInt("shouldMove", Input.GetMouseButton(0) ? 1 : 0);
-        var mousePosition = GetMousePosition();
-        computeShader.SetFloats("mousePosition", mousePosition);
-        computeShader.SetFloat("dt", Time.deltaTime);
-        computeShader.Dispatch(_kernelIndex, _warpCount, 1, 1);
-	}
-
-    float[] GetMousePosition()
-    {
-        var mp = Input.mousePosition;
-        var v = Camera.main.ScreenToWorldPoint(mp);
-        return new float[] { v.x, v.y };
-    }
-
-    void OnRenderObject()
-    {
-        material.SetPass(0);
-        Graphics.DrawProceduralNow(MeshTopology.Points, 1, particlesCount);
-    }
-
-    void OnDestroy()
-    {
-        if (_particlesBuffer != null)
-            _particlesBuffer.Release();
-    }
 }
